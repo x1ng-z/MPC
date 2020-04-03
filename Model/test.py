062eda48c4a1295364076966a7ba39bf38cd3f67
x1ng-z <37235318+x1ng-z@users.noreply.github.com>
2020-04-04 02:07:36 +0800

    change ff

--- Model/test.py
+++ Model/test.py
@@ -1,24 +1,8 @@
 import numpy as np
-m=2
-M=8
-B = np.zeros((m * M, m * M))
-for indexIn in range(m):
-    for noderow in range(M):
-        for nodecol in range(M):
-            if (nodecol <= noderow):
-                B[indexIn * M + noderow, indexIn * M + nodecol] = 1
-print(np.zeros(0))
-x=np.array([1,2,3,4,5,6,7,8])
-xm = x[1:-1]
-print(xm)
-
-xm_m1 = x[:-2]
-print(xm_m1)
-
-xm_p1 = x[2:]
-print(xm_p1)
-
-aa=np.array([[1,2],[3,4]])
-bb=np.array([[5,6],[7,8]])
-print(aa@bb)
-print(np.dot(aa ,bb))
+import sys
+if __name__=='__main__':
+    for i in range(0, len(sys.argv)):
+        strs = sys.argv[i]
+        print(i)
+        if i==2:
+            print(int(strs))
